c9e6cd53a7dc3f56915d3ccb9aafe1fb6e9c6a7f
hwangseong-in <gommpo111@gmail.com>
2020-06-06 00:17:11 +0900

    setup route for codeSplit

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Route, Switch} from 'react-router';
+// import loadable from '@loadable/component';
+
+
+function Routes() {
+
+    return (
+        <Switch>
+         //   <Route path="/" component={HomePage} exact/>
+        </Switch>
+    );
+};
+
+export default Routes;
+
+
