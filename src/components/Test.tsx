8c469baf32e69073d479f129e731ae34f531644b
sunginHwang <gommpo111@gmail.com>
2020-06-11 18:21:45 +0900

    huskey test commit

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 function Test() {
-    return (
-        <div>
-            <p>테스트 값</p>
-        </div>
-    )
+  return (
+    <div>
+      <p>테스트 값d</p>
+      <p>12</p>
+    </div>
+  );
 }
 
 export default Test;
