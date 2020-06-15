bc261e4fc272700cd9b58e05d003d2a31a2dbfaf
sunginHwang <gommpo111@gmail.com>
2020-06-15 18:47:58 +0900

    setup: use redux for container

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import TodoContainer from '../containers/TodoContainer';
 
 function todo() {
-
-    return(
-        <div>
-            todo페이지
-        </div>
-    )
+  return (
+    <div>
+      <TodoContainer />
+    </div>
+  );
 }
 
 export default todo;
