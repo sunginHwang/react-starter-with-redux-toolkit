748c1f77e77f7b8a9977859fe582a42204f19f7d
sunginHwang <gommpo111@gmail.com>
2020-06-15 18:50:09 +0900

    setup: remove default app page

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import {Route, Switch} from 'react-router';
+import { Route, Switch } from 'react-router';
 import loadable from '@loadable/component';
 
-const todo = loadable(() => import('../pages/Todo'))
+const todo = loadable(() => import('../pages/Todo'));
 
 function Routes() {
-
-    return (
-        <Switch>
-            <Route path="/todo" component={todo} exact/>
-        </Switch>
-    );
-};
+  return (
+    <Switch>
+      <Route path='/' component={todo} exact />
+    </Switch>
+  );
+}
 
 export default Routes;
-
-
