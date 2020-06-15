3746c54c2a1efde2f01619c4baa8bbb64f62191b
sunginHwang <gommpo111@gmail.com>
2020-06-15 18:51:48 +0900

    setup: lint-setup

--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -1,4 +1,4 @@
 export interface Todo {
-    title: string;
-    content: string;
+  title: string;
+  content: string;
 }
--- src/models/redux.ts
+++ src/models/redux.ts
@@ -1,5 +1,5 @@
 export type AsyncState<P> = {
-    loading: boolean;
-    data: P;
-    error?: any;
-}
+  loading: boolean;
+  data: P;
+  error?: any;
+};
